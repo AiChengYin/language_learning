79b575f50266fc895a46c6f3fac6738d23a8ef33
AiChengYin <yac524yinaicheng@gmail.com>
2025-05-11 00:47:12 +0900

    feat：設定ファイルを追加する

--- start/src/main/java/com/yinaicheng/Application.java
+++ start/src/main/java/com/yinaicheng/Application.java
@@ -1,17 +1,21 @@
 package com.yinaicheng;
-
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 /**
- * Spring Boot Starter
- *
- * @author Frank Zhang
+ * @author yinaicheng
  */
-@SpringBootApplication(scanBasePackages = {"com.yinaicheng", "com.alibaba.cola"})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@ServletComponentScan /*让项目扫描到Druidservlet，以便可以访问Druid连接池的UI页面*/
+@EnableAsync /* 启用异步 */
+@EnableCaching /* @EnableCaching开启缓存支持 */
+@EnableScheduling /* 启用定时任务的配置 */
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
